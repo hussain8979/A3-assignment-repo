2ebfff96626bef66ccb85723b21e985c23a927fb
hussain8979 <skhussain8979@gmail.com>
2024-01-24 21:56:28 +0600

    resolver of img section problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  module.exports = {
-    theme: {
-      extend: {
-        minHeight: {
-          '128': '32rem',
-          'img-height': '36rem',
-        }
-      }
-    }
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
